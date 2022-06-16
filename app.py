18855b0566547aaef5bee3ae777094004e8cb2ab
MD Saifuzzaman Sohan <107489012+MSSohan@users.noreply.github.com>
2022-06-17 03:43:02 +0600

    17.06.22

---
+++ app.py
@@ -0,0 +1,16 @@
+# import module
+from pdf2image import convert_from_path
+import cv2
+import numpy as np
+
+images = convert_from_path('Lab 1.pdf',500,poppler_path=r'C:\Program Files\Release-22.04.0-0\poppler-22.04.0\Library\bin')
+for i, image in enumerate(images):
+    fname = 'image'+str(i)+'.jpg'
+    image.save(fname, "PNG")
+    img = cv2.imread(fname)
+    # counting the number of pixels
+    number_of_white_pix = np.sum(img == 255)
+    number_of_black_pix = np.sum(img == 0)
+
+    print('Number of white pixels:', number_of_white_pix)
+    print('Number of black pixels:', number_of_black_pix)
